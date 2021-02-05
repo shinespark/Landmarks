4334be367ea5b4879ba14dcf95e7984521a8e304
shinespark <shnsprk@gmail.com>
2021-02-05 17:54:55 +0900

    Section 7 Pass Data into Child Views

--- Landmarks/Model/Landmark.swift
+++ Landmarks/Model/Landmark.swift
@@ -22,7 +22,7 @@ struct Landmark: Hashable, Codable, Identifiable {
     }
     
     private var coordinates: Coordinates
-    var locationCoodinate: CLLocationCoordinate2D {
+    var locationCoordinate: CLLocationCoordinate2D {
         CLLocationCoordinate2D(
             latitude: coordinates.latitude,
             longitude: coordinates.longitude
